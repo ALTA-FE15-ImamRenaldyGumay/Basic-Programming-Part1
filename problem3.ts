572835593617b1e4b9deb883fc04234c263305fa
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-07-06 19:55:19 +0700

    menambahkan problem 3, 4 dan 5

--- problem3.ts
+++ problem3.ts
@@ -1,1 +1,4 @@
-console.log("Hello World")
+// input nama
+console.log("Program Hello + Nama")
+let nama : string = "M. Imam Renaldy Gumay"
+console.log("Hello "+nama+"! Saya Javascript bahasa yang sangat menyenangkan")
